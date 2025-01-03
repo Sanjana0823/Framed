c24c620a020a1570f6ff4a90a8df2e8a8e9b2cac
Sanjana <asanjana0823@gmail.com>
2025-01-03 23:06:02 +0530

    Add logo to navbar

---
+++ frontend/src/Components/Assets/all_images.js
@@ -0,0 +1,10 @@
+import p1 from '../Assets/logo.jpg'
+
+let all_images = [
+
+    {
+        id: 1,
+        name: logo Image,
+        image: p1
+    }
+]
--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../Assets/logo.jpg'
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <div className='nav-logo'>
+        <img src={logo} alt='logo'/>
+      </div>
+      <ul className="nav-menu">
+        <li>Shop</li>
+        <li>New Arrivals</li>
+        <li>All products</li>
+        <li>Services</li>
+        <li>About</li>
+        <li>Contact Us</li>
+      </ul>
+      <div className="nav-login-cart">
+        <button>Login</button>
+        </div>
+    </div>
   )
 }
