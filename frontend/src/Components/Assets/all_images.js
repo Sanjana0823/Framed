e3c484d66df4c03088171bb8c7f8678bdc288b39
Sanjana <asanjana0823@gmail.com>
2025-01-06 20:35:18 +0530

    Add user and cart icons

--- frontend/src/Components/Assets/all_images.js
+++ frontend/src/Components/Assets/all_images.js
@@ -1,10 +1,23 @@
 import p1 from '../Assets/logo.jpg'
+import p2 from '../Assets/user_icon.png'
+import p3 from '../Assets/cart_icon.png'
+
 
 let all_images = [
 
     {
         id: 1,
-        name: logo Image,
+        name: "logo image",
         image: p1
+    },
+    {
+        id: 2,
+        name: "icon for cart",
+        image: p2
+    },    
+    {
+        id: 3,
+        name: "icon for user",
+        image: p3
     }
 ]
--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import './Navbar.css'
-import logo from '../Assets/logo.jpg'
+import React from "react";
+import "./Navbar.css";
+import logo from "../Assets/logo.jpg";
+import user_icon from "../Assets/user_icon.png";
+import cart_icon from "../Assets/cart_icon.png";
 
 export const Navbar = () => {
   return (
-    <div className='navbar'>
-      <div className='nav-logo'>
-        <img src={logo} alt='logo'/>
+    <div className="navbar">
+      <div className="nav-logo">
+        <img src={logo} alt="logo" />
       </div>
       <ul className="nav-menu">
         <li>Shop</li>
@@ -16,9 +18,14 @@ export const Navbar = () => {
         <li>About</li>
         <li>Contact Us</li>
       </ul>
-      <div className="nav-login-cart">
-        <button>Login</button>
+      <div className="icons">
+        <div className="nav-login-cart">
+          <img className="cart-icon" src={cart_icon} alt="cart icon" />
         </div>
+        <div className="nav-login-user">
+          <img className="user-icon" src={user_icon} alt="user icon" />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
